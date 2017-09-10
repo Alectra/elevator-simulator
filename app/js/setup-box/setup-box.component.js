455235729d431d749818714d64237438d52ef018
Alectra <roxdene@gmail.com>
2017-09-09 23:47:19 -0700

    Added minimums so that invalid values for number of floors or elevators or for elevator speed couldn't be too low.

--- app/js/setup-box/setup-box.component.js
+++ app/js/setup-box/setup-box.component.js
@@ -10,6 +10,13 @@
 			vm.simulation = null;
 			vm.submit = function () {
 				// vm.simData = vm.simulation.first + ' ' + vm.simulation.last;
+				if (vm.simulation.numFloors < 2)
+					vm.simulation.numFloors = 2;
+				if (vm.simulation.numElevators < 1)
+					vm.simulation.numElevators = 1;
+				if (vm.simulation.floorSpeed < 500)
+					vm.simulation.floorSpeed = 500;
+				// if (vm.simulation.numFloors > 1 && vm.simulation.numElevators > 0 && vm.simulation.floorSpeed > 500)
 				vm.simData = appService.submitSimulation(vm.simulation.numFloors, vm.simulation.numElevators, vm.simulation.floorSpeed);
 			};
 
